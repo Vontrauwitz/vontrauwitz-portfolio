fe6c37763cd50ee1e0795feb6a04e73bd5348cbe
Hans Trauwitz <hans.trauwitz@gmail.com>
2023-07-06 05:19:03 -0600

    cambios en about

--- src/pages/about.js
+++ src/pages/about.js
@@ -53,11 +53,14 @@ const About = () => {
           <div className='grid w-full grid-cols-8 gap-16 pb-10'>
             <div className='col-span-3 flex flex-col items-start justify-start'  >
               <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>Biography</h2>
-              <p className='font-medium'>Hi, I&apos;m CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, and user-centered digital experiences. With 4 years of experience in the field. I am always looking for new and innovative ways to bring my client&apos;s visions to life.
+              <p className='font-small'>
+                Hi, I&apos;m Hans, a Full Stack developer focused on creating robust and efficient digital experiences. I have a deep understanding of design principles, user experience, and the technical aspects of web development. I enjoy finding innovative solutions to complex problems and strive to deliver intuitive and seamless experiences for users.
               </p>
-              <p className=' my-4 font-medium'>I believe that design is about more than just making things look pretty - it&apos;s about solving problems and creating intuitive, enjoyable experiences for users.
+              <p className=' my-4 font-small'>
+                In my view, design goes beyond aesthetics. It involves problem-solving and crafting experiences that resonate with users. I firmly believe in a user-centered approach, always considering the target audience and their specific needs and goals. By leveraging this understanding, I can make informed design decisions that result in user-friendly products.
               </p>
-              <p className='font-medium'>Whether I&apos;m working on a website, mobile app, or other digital product, I bring my commitment to design excellence and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
+              <p className='font-small'>
+                I&apos;m excited to continue advancing my career as a Full Stack developer, utilizing my skills and passion to make meaningful contributions to the field. I look forward to the opportunity to collaborate with you on your next project and create exceptional digital solutions together.
               </p>
             </div>
             <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8'>
